04b1da668bffc6f527931b9ed57e55859a57321c
josuegraterol-Senai <josue.graterol@aluno.senai.br>
2026-05-09 15:35:43 -0300

    refactor: use environment variables for Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAeowjjxvbzLU2FgDQ6mtv1FxXuxTMkwu0",
-  authDomain: "encurtador-links-senai-94888.firebaseapp.com",
-  projectId: "encurtador-links-senai-94888",
-  storageBucket: "encurtador-links-senai-94888.firebasestorage.app",
-  messagingSenderId: "588576266962",
-  appId: "1:588576266962:web:eeb1129fb2ae79fdd82b0b",
-  measurementId: "G-Z2NPC1YKS6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
